7dcb38d36a1fcccd4c3feb4f8a0d4f880fc8769f
FJozsef99 <117368843+FJozsef99@users.noreply.github.com>
2022-11-13 16:26:56 +0100

    Initial push
    Class library created

---
+++ HNZ9CU_HFT_2022231/HNZ9CU_HFT_2022231.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HNZ9CU_HFT_2022231.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ HNZ9CU_HFT_2022231/HNZ9CU_HFT_2022231/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HNZ9CU_HFT_2022231
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
